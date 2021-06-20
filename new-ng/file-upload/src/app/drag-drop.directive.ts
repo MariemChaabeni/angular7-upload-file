230464e8fd3856493ecb6f12cd277bdc8c63aefe
Nick Vissari <networknerd@gmail.com>
2021-06-20 09:19:10 -0400

    Update for angular 12

--- new-ng/file-upload/src/app/drag-drop.directive.ts
+++ new-ng/file-upload/src/app/drag-drop.directive.ts
@@ -9,23 +9,24 @@ export class DragDropDirective {
   @HostBinding('style.background-color') private background = '#f5fcff'
   @HostBinding('style.opacity') private opacity = '1'
 
-  
   //Dragover listener
-  @HostListener('dragover', ['$event']) onDragOver(evt) {
+  @HostListener('dragover', ['$event']) onDragOver(evt: any) {
     evt.preventDefault();
     evt.stopPropagation();
     this.background = '#9ecbec';
     this.opacity = '0.8'
   }
+
   //Dragleave listener
-  @HostListener('dragleave', ['$event']) public onDragLeave(evt) {
+  @HostListener('dragleave', ['$event']) public onDragLeave(evt: any) {
     evt.preventDefault();
     evt.stopPropagation();
     this.background = '#f5fcff'
     this.opacity = '1'
   }
+
   //Drop listener
-  @HostListener('drop', ['$event']) public ondrop(evt) {
+  @HostListener('drop', ['$event']) public ondrop(evt: any) {
     evt.preventDefault();
     evt.stopPropagation();
     this.background = '#f5fcff'
